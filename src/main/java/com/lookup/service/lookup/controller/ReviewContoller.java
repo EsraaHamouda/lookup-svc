196a90c8f8d78b52458356bf0c1f1f9d7de72672
EsraaHamouda <esraahamouda94@gmail.com>
2020-07-24 19:23:46 +0200

    add paging to review listing

--- src/main/java/com/lookup/service/lookup/controller/ReviewContoller.java
+++ src/main/java/com/lookup/service/lookup/controller/ReviewContoller.java
@@ -4,10 +4,15 @@ import com.lookup.service.lookup.dao.ReviewRepository;
 import com.lookup.service.lookup.model.Review;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -23,6 +28,7 @@ public class ReviewContoller {
         reviewRepository.save(review);
         return new ResponseEntity<>(review, HttpStatus.OK);
     }
+
     @PutMapping("/{uuid}")
     public ResponseEntity<Review> updateReview(@RequestBody Review review, @PathVariable("uuid") String uuid) {
         review.setUuid(uuid);
@@ -42,11 +48,14 @@ public class ReviewContoller {
         return result.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).
                 orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
+
     @GetMapping
-    public ResponseEntity<Review> listReview(@PathVariable("uuid") String uuid) {
-        Optional<Review> result = reviewRepository.findById(uuid);
-        return result.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).
-                orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+    public ResponseEntity<List<Review>> listReview(@RequestParam("pageNumber") int pageNumber,
+                                                   @RequestParam("size") int size) {
+        Pageable pageable = PageRequest.of(pageNumber, size);
+         Page<Review> result = reviewRepository.findAll(pageable);
+        if (result.isEmpty()) new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(result.getContent(), HttpStatus.OK);
     }
 
 
--- src/main/java/com/lookup/service/lookup/dao/ReviewRepository.java
+++ src/main/java/com/lookup/service/lookup/dao/ReviewRepository.java
@@ -1,7 +1,11 @@
 package com.lookup.service.lookup.dao;
 
 import com.lookup.service.lookup.model.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ReviewRepository extends MongoRepository<Review, String> {
+    @Override
+    Page<Review> findAll(Pageable pageable);
 }
