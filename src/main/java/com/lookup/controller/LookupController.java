2375beacb3dc2705e72feef34a79ce696000ab9d
EsraaHamouda <esraahamouda94@gmail.com>
2020-08-22 22:59:46 +0200

    generic solution working version

--- src/main/java/com/lookup/controller/LookupController.java
+++ src/main/java/com/lookup/controller/LookupController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Api(tags = "Lookups APIs")
-@RestController("/")
+@RestController
 @RequestMapping("/lookup")
 
 public class LookupController {
